1b9407bf732bfa885d62e6a7316a274268638a87
stalmf01 <matt.stallman.2218@gmail.com>
2019-05-19 17:09:11 -0400

    fixed advice

--- ElviraBot.py
+++ ElviraBot.py
@@ -6,19 +6,17 @@ from discord.ext import commands
 
 class ElviraCog(commands.Cog):
     event_flag = 0
+    choices = []
 
     def __init__(self, bot):
         self.bot = bot
+        with open('advice.txt', 'r') as advice_file:
+            for line in advice_file:
+                self.choices.append(line)
 
     @commands.command()
     async def advice(self, ctx):
-        choices = []
-
-        with open('advice.txt', 'r') as advice_file:
-            for line in advice_file:
-                choices.append(line)
-
-            await ctx.send(random.choice(choices))
+        await ctx.send(random.choice(self.choices))
 
     @commands.Cog.listener()
     async def on_ready(self):
