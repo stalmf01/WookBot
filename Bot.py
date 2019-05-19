e26a1ccaefbe072054ae64a4aaf81b07b0dd2372
stalmf01 <matt.stallman.2218@gmail.com>
2019-05-19 16:16:36 -0400

    added prefix from tokens file

--- Bot.py
+++ Bot.py
@@ -2,9 +2,11 @@ import discord
 from discord.ext import commands
 
 
-prefix = '!'
 with open('Tokens.txt', 'r') as tokens_file:
     token = tokens_file.readline().strip()
+    next(tokens_file)
+    next(tokens_file)
+    prefix = tokens_file.readline().strip()
 
 bot = commands.Bot(command_prefix=prefix, description='Bot of many things')
 
