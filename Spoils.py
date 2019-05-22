ecbae7d5563ab5749823a6c394f6a9ee74e28f85
stalmf01 <matt.stallman.2218@gmail.com>
2019-05-21 23:03:30 -0400

    tidying up

--- Spoils.py
+++ Spoils.py
@@ -8,12 +8,7 @@ from datetime import datetime
 class Spoiler(commands.Cog):
     earliest_date = datetime(year=2019, month=5, day=22)
     month_dict = {
-                "Janurary": 1,
-                "Febuary": 2,
-                "March": 3,
-                "April": 4,
-                "May": 5,
-                "June": 6,
+                "January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6,
                 "July": 7, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12
                 }
 
