ce5c6dc72bd54d792a43a82aede0579fc0a0facf
stalmf01 <matt.stallman.2218@gmail.com>
2019-05-21 22:25:16 -0400

    code is currently broken

--- Spoils.py
+++ Spoils.py
@@ -27,13 +27,16 @@ class Spoiler(commands.Cog):
         await self.feed_poll()
         print('here')
 
+    # count down loop
     async def feed_poll(self):
         await self.bot.wait_until_ready()
         while True:
             self.spoiler_feed = feedparser.parse("https://www.mtgsalvation.com/spoilers.rss")
             self.entry = self.spoiler_feed.entries[:10]
-            for spoil in self.entry:
-                unparsed_date = spoil.published.split()
+            print(self.entry[0])
+            index = 9
+            while index >= 0:
+                unparsed_date = self.entry[index].published.split()
                 day = int(unparsed_date[1])
                 month = self.month_dict[unparsed_date[2]]
                 year = int(unparsed_date[3])
@@ -42,14 +45,20 @@ class Spoiler(commands.Cog):
                 min = int(time[1])
                 sec = int(time[2])
                 total_time = hour*360 + min*60 + sec
-                if month >= self.earliest_month and day >= self.earliest_day and year >= self.earliest_year \
-                        and total_time >= self.earliest_total_time:
-                    self.earliest_day = day
-                    self.earliest_month = month
-                    self.earliest_year = year
-                    self.earliest_total_time = total_time+1
-                    channel = self.bot.get_channel(532305526969860096)
-                    await channel.send(spoil.link)
+                print(str(hour) + ' ' + str(min) + ' ' + str(sec))
+                print(str(month) + ' ' + str(day))
+                print(total_time)
+                print(self.earliest_total_time)
+                if month >= self.earliest_month and day >= self.earliest_day and year >= self.earliest_year:
+                    if total_time > self.earliest_total_time:
+                        self.earliest_day = day
+                        self.earliest_month = month
+                        self.earliest_year = year
+                        self.earliest_total_time = total_time
+                        channel = self.bot.get_channel(576436682879008780)
+                        print((self.entry[index].link))
+                        # await channel.send(self.entry[index].link)
+                        index -= 1
             await asyncio.sleep(50)
     """""
     @commands.command()
@@ -58,6 +67,7 @@ class Spoiler(commands.Cog):
             await ctx.send(spoil.published)
             print(spoil.published)
     """
+
 
 def setup(bot):
     bot.add_cog(Spoiler(bot))
